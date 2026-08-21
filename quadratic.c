45eac90531fb49bc0facff87955e4f3d9cce1b2b
Sweta-Codess <its.sweta008@gmail.com>
2026-08-21 23:33:16 +0530

    Add files via upload

---
+++ quadratic.c
@@ -0,0 +1,52 @@
+//Q17: Write a program to find the roots of a quadratic equation and categorize them.
+
+/*
+Sample Test Cases:
+Input 1:
+1 -3 2
+Output 1:
+Roots are real and different: 2, 1
+
+Input 2:
+1 -2 1
+Output 2:
+Roots are real and same: 1
+
+Input 3:
+1 2 5
+Output 3:
+Roots are complex
+
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+     float a,b,c,root1,root2,root,d;
+    
+     printf("ENTER A,B and C: \n");
+     scanf("%f %f %f",&a,&b,&c);
+
+     d=b*b-4*a*c;
+
+     if(d>0)
+          {
+                 root1=(-b+sqrt(d))/(2*a);
+                 root2=(-b-sqrt(d))/(2*a);
+
+            printf("%.0f , %.0f: ROOTS ARE DIFFERENT \n",root1,root2);
+          }
+      else if(d==0)
+         {
+              root=-b/2*a;
+             printf("%.0f: ROOTS ARE SAME \n",root);
+
+         }
+      else
+           printf("ROOTS ARE COMPLEX \n");
+
+
+return 0;
+}
